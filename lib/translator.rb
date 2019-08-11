4685fc0d23774bcbb20196d81de4dc3cb8c77d53
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-11 14:35:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
     "get_emoticon" => {}
   }
   emoticons.each do |key, value|
-    new_emote_hash["get_meaning"][value[1]]
+    new_emote_hash["get_meaning"][value[1]] = key
   end
   return new_emote_hash
 end
