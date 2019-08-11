bbf59c9f092a1ea94c7093c1ab0d506c25612103
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-11 15:08:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,5 +18,10 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)
   returned_emoticon = load_library(file_path)["get_emoticon"][emoticon]
+  if returned_emoticon = nil 
+    return "Sorry, that emoticon was not found"
+  end
+
+returned_emoticon
 end
 
