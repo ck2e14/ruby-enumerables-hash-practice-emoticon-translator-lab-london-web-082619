98cd387d61be9c2bd70de10ea08286e461aab16f
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-11 15:05:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,5 +15,7 @@ end
 #here you you iterating over everything in the emoticons .yml file, having assigned the keys to be called 'emote_name' and the values of those keys to be called 'corresponding_emote_array'. You're then adding what is found at index 1 (the japanese emotes) of those arrays to be the keys of the nested hash you earlier created, 'get_meaning' and assigning the values of these newly created keys to be the name of the emotes, i.e. emote_name, which is a string.
 #next you are doing a similar thing with the nested hash 'get_emoticon' which takes the english emote at index 0 of the 'corresponding_emote_array's to be the keys of this new hash and then assings the values of these new keys to be whatever is found at index 1 of the return value of 'emote_name', which is always the japanese emote. 
 
-
+def get_japanese_emoticon(file_path, emoticon)
+  load_library(file_path)
+end
 
